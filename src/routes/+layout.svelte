57a620d975bb909e9c690dc7e40f1176a3508f48
Henry Pick <1henrypick@gmail.com>
2023-08-17 10:13:40 -0700

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Header from './Header.svelte';
+    import '../app.css';
+    import Gradient from './Gradient.svelte';
+</script>
+
+<div class="app">
+    <Header />
+    <Gradient />
+    <main>
+        <slot />
+    </main>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<div class="title-box">
+	<div class="name">Henry Pick</div>
+	<div class="portfolio">Work and academic portfolio</div>
+	<div class="info">
+		I am a recent graduate of Harvey Mudd College and aspiring machine learning engineer.
+		<br />
+		On this site, you'll find a showcase of my independent projects as well other things about me.
+	</div>
+</div>
+
+<style>
+	.title-box {
+		position: relative;
+	}
+	.name {
+		font-family: 'Times New Roman', Times, serif;
+		color: black;
+	}
+</style>
