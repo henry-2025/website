696ffbc86f3e298ca62a7a4c38f66200ba863b68
Henry Pick <1henrypick@gmail.com>
2024-08-03 09:15:04 -0700

    path routing for local static content

--- src/routes/projects/+page.server.js
+++ src/routes/projects/+page.server.js
@@ -2,29 +2,32 @@ import { read } from "$app/server";
 import Markdoc from "@markdoc/markdoc";
 import yaml from "js-yaml";
 
-const articles = Object.values(
+const articlePaths = Object.entries(
   import.meta.glob("./pages/*.md", {
     query: "?url",
     eager: true,
   }),
-).map((x) => x.default);
+).map((entry) => {
+  let file = entry[0].replace(/^.*[\\/]/, "");
+  file = file.substr(0, file.length - 3);
+  return { file: file, path: entry[1].default };
+});
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
   /** @type Array<Object> */
   let data = [];
-  for (let i = 0; i < articles.length; i++) {
-    const a = articles[i];
-    const postSource = await read(a).text();
+  for (let i = 0; i < articlePaths.length; i++) {
+    const postSource = await read(articlePaths[i].path).text();
     const ast = Markdoc.parse(postSource);
     const frontMatter = yaml.load(ast.attributes.frontmatter);
-    const content = Markdoc.transform(ast /* config */);
-    console.log(a.match(`/\/.*.md/g`));
+    //const content = Markdoc.transform(ast /* config */);
+    //const html = Markdoc.renderers.html(content);
     data.push({
       title: frontMatter.title,
       description: frontMatter.description,
       date: frontMatter.date,
-      path: a.match("/.*.md"),
+      path: articlePaths[i].file,
     });
   }
   return { articles: data };
--- src/routes/projects/[slug]/+page.server.js
+++ src/routes/projects/[slug]/+page.server.js
@@ -2,9 +2,22 @@ import { read } from "$app/server";
 import Markdoc from "@markdoc/markdoc";
 import render from "./renderer";
 
+const articles = import.meta.glob("../pages/*.md", {
+  query: "?url",
+  eager: true,
+});
+const pathFromArticleName = new Map(
+  Object.entries(articles).map((e) => {
+    let file = e[0].replace(/^.*[\\/]/, "");
+    file = file.substr(0, file.length - 3);
+    return [file, e[1].default];
+  }),
+);
+
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
-  const postSource = await read(`../pages/${params.slug}.md`).text();
+  const file = pathFromArticleName.get(params.slug);
+  const postSource = await read(file).text();
   const ast = Markdoc.parse(postSource);
   const content = Markdoc.transform(ast /* config */);
   const html = render(content);
